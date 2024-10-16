5e79b475320fb4f23ed07a7b50126fbe1d3dbdcf
lsfresta770 <lsfresta770@outlook.com>
2024-10-17 05:44:13 +1000

    Create ao.js

---
+++ ao.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+
+$("a:contains('Login')").attr("href", "http://www.google.com/"); $("a:contains('Race Registration')").attr("href", "http://www.google.com/"); $("a:contains('Renewing your Membership')").attr("href", "http://www.google.com/"); $("a:contains('Want to become a Member')").attr("href", "http://www.google.com/");
+
+
+});
